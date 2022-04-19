405c50d8b571964894de600f5c86815be6d77030
Vajreshwari Chougule <v.chougule@zensar.com>
2022-04-19 19:45:37 +0530

    ProgramDay2

---
+++ AssignmentNo1/AssignmentNo1/AssignmentNo1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AssignmentNo1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssignmentNo1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e463757-1906-444a-adb3-1681c99f46ed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PracticeDay1/PracticeDay1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeDay1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter user name");
+            string username = Console.ReadLine();
+            Console.WriteLine("Hi " + username + " Welcome to the world of C#");
+
+            int num;
+            Console.WriteLine("Enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+                Console.WriteLine("It is even number");
+            else
+                Console.WriteLine("It is odd number");
+
+            Console.WriteLine("Enter the num1");
+            int n1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the num2");
+            int n2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the num3");
+            int n3=Convert.ToInt32(Console.ReadLine());
+            if (n1 > n2 && n2 > n3)
+            {
+                Console.WriteLine("the largest " + n1);
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                Console.WriteLine("the 1st largest" + n2);
+            }
+            else
+            {
+                Console.WriteLine("the 3rd largest is" + n3);
+            }
+            Console.Read();
+        }
+    }
+}
